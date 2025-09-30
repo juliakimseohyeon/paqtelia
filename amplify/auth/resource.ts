08bbc0c80d6d3c415c023016baa72fdda7dd1b62
Julia Kim <juliakimseohyeon@gmail.com>
2025-09-29 20:45:16 -0700

    Comment out external providers except for facebook

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -8,29 +8,26 @@ export const auth = defineAuth({
 	loginWith: {
 		email: true,
 		externalProviders: {
-			signInWithApple: {
-				clientId: secret("SIWA_CLIENT_ID"),
-				teamId: secret("SIWA_TEAM_ID"),
-				keyId: secret("SIWA_KEY_ID"),
-				privateKey: secret("SIWA_PRIVATE_KEY"),
-			},
-			loginWithAmazon: {
-				clientId: secret("LOGINWITHAMAZON_CLIENT_ID"),
-				clientSecret: secret("LOGINWITHAMAZON_CLIENT_SECRET"),
-			},
+			// signInWithApple: {
+			// 	clientId: secret("SIWA_CLIENT_ID"),
+			// 	teamId: secret("SIWA_TEAM_ID"),
+			// 	keyId: secret("SIWA_KEY_ID"),
+			// 	privateKey: secret("SIWA_PRIVATE_KEY"),
+			// },
+			// loginWithAmazon: {
+			// 	clientId: secret("LOGINWITHAMAZON_CLIENT_ID"),
+			// 	clientSecret: secret("LOGINWITHAMAZON_CLIENT_SECRET"),
+			// },
 			facebook: {
 				clientId: secret("FACEBOOK_CLIENT_ID"),
 				clientSecret: secret("FACEBOOK_CLIENT_SECRET"),
 			},
-			google: {
-				clientId: secret("GOOGLE_CLIENT_ID"),
-				clientSecret: secret("GOOGLE_CLIENT_SECRET"),
-			},
-			callbackUrls: [
-				"http://localhost:3000/profile",
-				"https://mywebsite.com/profile",
-			],
-			logoutUrls: ["http://localhost:3000/", "https://mywebsite.com"],
+			// google: {
+			// 	clientId: secret("GOOGLE_CLIENT_ID"),
+			// 	clientSecret: secret("GOOGLE_CLIENT_SECRET"),
+			// },
+			callbackUrls: ["http://localhost:3000/profile"],
+			logoutUrls: ["http://localhost:3000/"],
 		},
 	},
 });
