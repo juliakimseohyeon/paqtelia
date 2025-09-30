9d41d0d4a61ae499ec6004465ad3c867d5a2fe17
Julia Kim <juliakimseohyeon@gmail.com>
2025-09-30 10:18:51 -0700

    Add social login buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
+import outputs from "../amplify_outputs.json";
+import "@aws-amplify/ui-react/styles.css";
 
 const client = generateClient<Schema>();
+Amplify.configure(outputs);
 
 function App() {
 	const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
@@ -18,22 +23,31 @@ function App() {
 	}
 
 	return (
-		<main>
-			<h1>My todos</h1>
-			<button onClick={createTodo}>+ new</button>
-			<ul>
-				{todos.map((todo) => (
-					<li key={todo.id}>{todo.content}</li>
-				))}
-			</ul>
-			<div>
-				🥳 App successfully hosted. Try creating a new todo.
-				<br />
-				<a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-					Review next step of this tutorial.
-				</a>
-			</div>
-		</main>
+		<Authenticator socialProviders={["amazon", "apple", "facebook", "google"]}>
+			{({ signOut, user }) => (
+				<main>
+					<h1>{user?.username} todos</h1>
+					<button type="button" onClick={createTodo}>
+						+ new
+					</button>
+					<ul>
+						{todos.map((todo) => (
+							<li key={todo.id}>{todo.content}</li>
+						))}
+					</ul>
+					<div>
+						🥳 App successfully hosted. Try creating a new todo.
+						<br />
+						<a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+							Review next step of this tutorial.
+						</a>
+					</div>
+					<button type="button" onClick={signOut}>
+						Sign out
+					</button>
+				</main>
+			)}
+		</Authenticator>
 	);
 }
 
